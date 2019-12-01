7e56e0ea91285aa8a6f212d3ba480d0561108307
tebogoditse <tebogoditse2@gmail.com>
2019-12-01 23:29:49 +0200

    second-commit

--- postgresql.sql
+++ postgresql.sql
@@ -6,7 +6,7 @@ CREATE DATABASE Umuzi
 
 //Customer Table
 create table Customers(
-customer_id serial PRIMARY KEY,
+customer_id INT SERIAL PRIMARY KEY,
 FirstName VARCHAR (50),
 LastName VARCHAR (50),
 Gender VARCHAR,
@@ -19,72 +19,73 @@ Country VARCHAR (50)
 
 //Inserting values into table
 INSERT INTO Customers(customerID, FirstName, LastName, Gender, Address, Phone, Email, City, Country)
-VALUES(1, 'John', 'Hibert', 'Male', '284 chaucer st', 084789657, 'john@gmail.com', 'Johannesburg', 'South Africa');
-INSERT INTO Customers(customerID, FirstName, LastName, Gender, Address, Phone, Email, City, Country)
-VALUES(2, 'Thando', 'Sithole', 'Female', '240 Sect 1', 0794445584, 'thando@gmail.com', 'Cape Town', 'South Africa');
-INSERT INTO Customers(customerID, FirstName, LastName, Gender, Address, Phone, Email, City, Country)
-VALUES(3, 'Leon', 'Glen', 'Male', '81 Everton Rd, Gillits', 0820832830, 'leaon@gmail.com', 'Durban', 'South Africa');
-INSERT INTO Customers(customerID, FirstName, LastName, Gender, Address, Phone, Email, City, Country)
-VALUES(4, 'Charl', 'Muller', 'Male', '290 Dorset Ecke', +44856872553, 'Charl.muller@yahoo.com', 'Berlin', 'Germany');
-INSERT INTO Customers(customerID, FirstName, LastName, Gender, Address, Phone, Email, City, Country)
-VALUES(5, 'Julia', 'Stein', 'Female', '2 Wernerring', +448672445058, 'Js234@yahoo.com', 'Frankfurt', 'Germany');
+VALUES('John', 'Hibert', 'Male', '284 chaucer st', 084789657, 'john@gmail.com', 'Johannesburg', 'South Africa');
+INSERT INTO Customers(FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES('Thando', 'Sithole', 'Female', '240 Sect 1', 0794445584, 'thando@gmail.com', 'Cape Town', 'South Africa');
+INSERT INTO Customers(FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES('Leon', 'Glen', 'Male', '81 Everton Rd, Gillits', 0820832830, 'leaon@gmail.com', 'Durban', 'South Africa');
+INSERT INTO Customers(FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES('Charl', 'Muller', 'Male', '290 Dorset Ecke', +44856872553, 'Charl.muller@yahoo.com', 'Berlin', 'Germany');
+INSERT INTO Customers(FirstName, LastName, Gender, Address, Phone, Email, City, Country)
+VALUES('Julia', 'Stein', 'Female', '2 Wernerring', +448672445058, 'Js234@yahoo.com', 'Frankfurt', 'Germany');
 
 ---------------------------------------------------------------------------------------------------------------------------
 
 //Employees Table
 CREATE TABLE Employees(
 EmployeeID serial PRIMARY KEY,
-FirstName VARCHAR (50),
-LastName VARCHAR (50),
-Email VARCHAR (100),
-JobTitle VARCHAR (20)
+FirstName VARCHAR (50) NOT NULL,
+LastName VARCHAR (50) NOT NULL,
+Email VARCHAR (100) NOT NULL,
+JobTitle VARCHAR (20) NOT NULL
 );
 
 //Inserting values into table
-INSERT INTO Employees(EmployeeID, FirstName, LastName, Email, JobTitle)
-VALUES (1,'Kani','Matthew','mat@gmail.com','Manager');
-INSERT INTO Employees(EmployeeID, FirstName, LastName, Email, JobTitle)
-VALUES (2,'Lesly','Cronje','LesC@gmail.com','Clerk');
-INSERT INTO Employees(EmployeeID, FirstName, LastName, Email, JobTitle)
-VALUES (3,'Gideon','Maduku','m@gmail.com','Accountant');
+INSERT INTO Employees(FirstName, LastName, Email, JobTitle)
+VALUES ('Kani','Matthew','mat@gmail.com','Manager');
+INSERT INTO Employees(FirstName, LastName, Email, JobTitle)
+VALUES ('Lesly','Cronje','LesC@gmail.com','Clerk');
+INSERT INTO Employees(FirstName, LastName, Email, JobTitle)
+VALUES ('Gideon','Maduku','m@gmail.com','Accountant');
 ----------------------------------------------------------------------------------------------------------------------------
 
 //Orders Table
 CREATE TABLE Orders (
-    OrderID serial PRIMARY KEY NOT NULL,
+    OrderID SERIAL,
     ProductID INT NOT NULL,
-    PaymentID INT NOT NULL,
+    PaymentID SERIAL,
     FulfilledByEmployeeID INT NOT NULL,
     DateRequired DATE NOT NULL,
     DateShipped DATE,
-    status VARCHAR(20)
+    status VARCHAR(20),
+    PRIMARY KEY (OrderID, PaymentID)
 )
 
 //Inserting values in the table
 
-INSERT INTO Orders (OrderID,ProductID,PaymentID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
-VALUES (1,1,1,2,'05-09-2018', NULL,'Not shipped');
-INSERT INTO Orders (OrderID,ProductID,PaymentID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
-VALUES (2,1,2,2,'04-09-2018','03-09-2018','Shipped');
-INSERT INTO Orders (OrderID,ProductID,PaymentID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
-VALUES (3,3,3,3,'06-09-2018', NULL,'Not shipped');
+INSERT INTO Orders (ProductID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
+VALUES (1,2,'05-09-2018', NULL,'Not shipped');
+INSERT INTO Orders (ProductID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
+VALUES (1,2,'04-09-2018','03-09-2018','Shipped');
+INSERT INTO Orders (ProductID,FulfilledByEmployeeID,DateRequired ,DateShipped,status)
+VALUES (3,3,'06-09-2018', NULL,'Not shipped');
 ----------------------------------------------------------------------------------------------------------------------------
 
 //Payments
 CREATE TABLE Payments(
-    CustomerId serial PRIMARY KEY NOT NULL,
-    PaymentID INT NOT NULL,
-    PaymentDate DATE NOT NULL, 
-    Amount DECIMAL
+   CustomerID     INT        REFERENCES Customers(CustomerID),
+   PaymentID      SERIAL     PRIMARY KEY,
+   PaymentDate    DATE        NOT NULL,
+   Amount         NUMERIC    NOT NULL
 );
 
 //Inserting values into table
-INSERT INTO Payments(CustomerId, PaymentID, PaymentDate, Amount)
-VALUES (1,1,'01-09-2018', 150.75);
-INSERT INTO Payments(CustomerId, PaymentID, PaymentDate, Amount)
-VALUES (5,2,'03-09-2018',150.75);
-INSERT INTO Payments(CustomerId, PaymentID, PaymentDate, Amount)
-VALUES (4,3,'03-09-2018',700.60);
+INSERT INTO Payments(CustomerId, PaymentDate, Amount)
+VALUES (1, '2018-09-01', 150.75);
+INSERT INTO Payments(CustomerId, PaymentDate, Amount)
+VALUES (5, '2018-09-02',150.75);
+INSERT INTO Payments(CustomerId, PaymentDate, Amount)
+VALUES (4, '2018-09-03' ,700.60);
 ---------------------------------------------------------------------------------------------------------------------------
 
 //Products Table
@@ -92,16 +93,16 @@ CREATE TABLE Products(
     ProductId serial PRIMARY KEY NOT NULL,
     ProductName VARCHAR (100),
     Desrciption VARCHAR (300),
-    BuyPrice DECIMAL
+    BuyPrice NUMERIC NOT NULL
 )
 
 //Inserting values into table
-INSERT INTO Products(ProductId, ProductName, Desrciption, BuyPrice)
-VALUES (1,'Harley Davidson Chopper', 'This replica features working kickstand, front suspension, gear-shift lever',150.75);
-INSERT INTO Products(ProductId, ProductName, Desrciption, BuyPrice);
-VALUES (2,'Classic Car','Turnable front wheels, steering function',550.75);
-INSERT INTO Products(ProductId, ProductName, Desrciption, BuyPrice)
-VALUES (3,'Sports car','Turnable front wheels, steering function',700.60);
+INSERT INTO Products(ProductName, Desrciption, BuyPrice)
+VALUES ('Harley Davidson Chopper', 'This replica features working kickstand, front suspension, gear-shift lever',150.75);
+INSERT INTO Products(ProductName, Desrciption, BuyPrice)
+VALUES ('Classic Car','Turnable front wheels, steering function',550.75);
+INSERT INTO Products(ProductName, Desrciption, BuyPrice)
+VALUES ('Sports car','Turnable front wheels, steering function',700.60);
 ---------------------------------------------------------------------------------------------------------------------------
 
 
